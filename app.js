25b74e7817e0e211d0b34aa5ad9686bacb1b84a8
Gonzalo Hernandez JR <chaloh01@Gonzalos-MacBook-Pro.local>
2022-06-20 19:14:30 -0700

    added syntax to app.js file

---
+++ app.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+
+const generatePage = require('./src/page-template.js');
+
+const inquirer = require('inquirer');
+
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+
+//const pageHTML = generatePage(name, github);
+
+//fs.writeFile('./index.html', pageHTML, err => {
+  //if (err) throw err;
+
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
+inquirer
+  .prompt([
+    {
+      type: 'input', 
+      name: 'name',
+      message: 'what is your name?' 
+
+    }
+  ])
+   .then(answers => console.log(answers))
